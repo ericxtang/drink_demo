1e6b83645e88a5869a54aaa2cd87bea807beaccd
ericxtang <eric.x.tang@gmail.com>
2011-07-23 14:30:33 -0400

    call the search endpoint with a general query instead of tag

--- around_me.rb
+++ around_me.rb
@@ -9,7 +9,7 @@ get '/basketball' do
   @client_secret = "yy9xtwpWAbDZoVcIB8pzeO6vWuNk75wvFI5ibUyA"
   @address = "915 Broadway, New York"
   @tags = ["basketball"]
-  response = HTTParty.get("https://api.hyperpublic.com/api/v1/people?client_id=#{@client_id}&client_secret=#{@client_secret}&address=#{CGI::escape(@address)}&tags=#{@tags.join(",")}&limit=11")
+  response = HTTParty.get("https://api.hyperpublic.com/api/v1/people?client_id=#{@client_id}&client_secret=#{@client_secret}&address=#{CGI::escape(@address)}&q=#{@tags.join(" ")}&limit=11")
   @objects = JSON.parse(response.body)
   haml :people, :format => :html5
 end
@@ -19,7 +19,7 @@ get '/burgers' do
   @client_secret = "yy9xtwpWAbDZoVcIB8pzeO6vWuNk75wvFI5ibUyA"
   @address = "915 Broadway, New York"
   @tags = ["burgers"]
-  response = HTTParty.get("https://api.hyperpublic.com/api/v1/places?client_id=#{@client_id}&client_secret=#{@client_secret}&address=#{CGI::escape(@address)}&tags=#{@tags.join(",")}&limit=11&with_photo=true")
+  response = HTTParty.get("https://api.hyperpublic.com/api/v1/places?client_id=#{@client_id}&client_secret=#{@client_secret}&address=#{CGI::escape(@address)}&q=#{@tags.join(" ")}&limit=11&with_photo=true")
   @objects = JSON.parse(response.body)
   haml :places, :format => :html5
 end
